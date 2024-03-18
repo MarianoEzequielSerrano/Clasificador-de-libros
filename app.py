7f09e61eb7ee1b244461f15edbdba5b9f895f6f9
renatacatelli <renatacatelli15@gmail.com>
2024-03-18 12:11:34 -0300

    Agregando metodo GET para clasificacion del libro

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import joblib  # Para cargar el modelo entrenado
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -30,5 +31,45 @@ def recomendar_libros():
         print(f"Error: {e}")
         return jsonify({'error': str(e)}), 500  # Devuelve el error 500 con detalles
 
+if __name__ == '__main__':
+    app.run(debug=True)
+
+#Definicion de la ruta para manejar solicitudes GET para obtener la clasificacion del libro por su ID
+
+def obtener_clasificacion_libro(libro_index):
+    #Cargar el csv
+    df = pd.read_csv('books_df_final.csv')
+
+    #Verificar si el indice está dentro del rango de indice del DataFrame
+    if libro_index < len(df):
+        #Obtener la fila que corresponde al indice dado
+        libro = df.iloc[libro_index]
+
+        #Obtener la clasificacion del libro
+        clasificacion_fiction = libro['Genre_Fiction']
+        clasificacion_non_fiction = libro['Genre_Non Fiction']
+
+    #Verificar la clasificacion del libro y devolverla como resultado
+        if clasificacion_fiction == 1:
+            return 'Fiction'
+        elif clasificacion_non_fiction == 0:
+            return 'Non Fiction'
+        else:
+            return 'Clasificacion Desconocida'
+    else:
+        return 'Indice fuera de rango'
+    
+@app.route('/libro/<int:libro_id>', methods=['GET'])    
+def obtener_clasificacion(libro_id):
+    try:
+        # Llamo a la funcion para obtener la clasificacion del libro
+        clasificacion = obtener_clasificacion_libro(libro_id)
+
+        #Devolver la clasificacion del libro como parte de la respuesta
+        return jsonify({'ID': libro_id, 'Clasificacion': clasificacion})
+    
+    except Exception as e:
+        return jsonify ({'error': str(e)}), 500
+    
 if __name__ == '__main__':
     app.run(debug=True)
