f688f54b2fc77d01bf24968dee3b3f50ad511569
Luciana Danise <luulidanise@gmail.com>
2024-10-02 18:01:58 -0300

    diseño de frontend

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify
+import json
+from flask import Flask, request, jsonify, render_template #type: ignore
 import joblib  # Para cargar el modelo entrenado
 
 app = Flask(__name__)
@@ -8,27 +9,32 @@ modelo_recomendacion = joblib.load('gbm_model.pkl')
 
 @app.route('/')
 def index():
-    return "¡Hola, esta es la página principal!"
+    return render_template('index.html')
 
 @app.route('/recomendar-libros', methods=['POST'])
 def recomendar_libros():
     try:
-        data = request.get_json()
+        data = request.form
 
         # Obtener los datos del usuario de la solicitud
-        usuario_data = [data['User Rating'], data['Reviews'], data['Price'], data['Year']]
+
+        usuario_data = [float(data['rating']), float(data['review']), float(data['price']), int(data['year'])]
 
         # Realizar la predicción con el modelo de recomendación
         recomendacion = modelo_recomendacion.predict([usuario_data])[0]
 
         # Convertir el resultado a un tipo serializable
-        recomendacion_serializable = int(recomendacion)
+        recomendacion_serializable = int(recomendacion) 
 
-        return jsonify({'libro_recomendado': recomendacion_serializable})
+        return render_template ('new.html', recomendado=recomendacion_serializable, data=data)
 
     except Exception as e:
         print(f"Error: {e}")
         return jsonify({'error': str(e)}), 500  # Devuelve el error 500 con detalles
+
+@app.route('/Recomendacion')
+def predict():
+    return render_template('new.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
